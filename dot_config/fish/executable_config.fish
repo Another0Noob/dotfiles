d250bd174c2685d49412bbe629011fbe8091b703
Another0Noob <25Antonym02+github@gmail.com>
2026-03-25 14:09:25 +0100

    fix fish

--- dot_config/fish/executable_config.fish
+++ dot_config/fish/executable_config.fish
@@ -11,7 +11,7 @@ function apply-my-theme --on-variable=my_theme
     fish_config theme choose $my_theme
 end
 
-set -U my_theme Catppuccin\ Mocha
+set -U my_theme catppuccin-mocha
 
 if status is-interactive # Commands to run in interactive sessions can go here
 
